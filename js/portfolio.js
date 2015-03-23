affde2a45eb78a46a7466729fc74887567bcc059
Andre Vitorio <andre@vitorio.net>
2015-03-23 12:57:06 +0000

    Fix responsiveness of portfolio gallery

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,23 +2,26 @@
  $('.gallery ul li a').click(function() {
      var itemID = $(this).attr('href');
      $('.gallery ul').addClass('item_open');
+     $('.gallery').addClass('gallery-height');
      $(itemID).addClass('item_open');
      return false;
  });
  $('.close').click(function() {
      $('.port, .gallery ul').removeClass('item_open');
+     $('.gallery').removeClass('gallery-height');
      return false;
  });
 
 $(document).click(function(event) { 
     if(!$(event.target).closest('.port').length) {
         $('.port, .gallery ul').removeClass('item_open');
+        $('.gallery').removeClass('gallery-height');
         return false;
     }        
 })
 
  $(".gallery ul li a").click(function() {
      $('html, body').animate({
-         scrollTop: parseInt($("#top").offset().top)
+         scrollTop: parseInt($("#hope").offset().top)
      }, 400);
  });
