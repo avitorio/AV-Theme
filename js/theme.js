d8cc1492f6329a306567bb9e4aabe436196f8be2
Andre Vitorio <andre@vitorio.net>
2015-04-02 15:08:42 +0100

    Fixed responsive footer icons

--- js/theme.js
+++ js/theme.js
@@ -1,19 +1,3 @@
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
 // Show navbar only after the first Section
 (function ($) {
   $(document).ready(function(){
@@ -34,6 +18,22 @@ $('.navbar-collapse ul li a').click(function() {
   });
 });
   }(jQuery));
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
 
  // Portfolio
  $('.gallery ul li a').click(function() {
